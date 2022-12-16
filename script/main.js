a6890d8e523fab8fc1f0893f1ba82c39a0801426
MBrayan25 <micael.brayan.s@gmail.com>
2022-12-16 18:18:13 -0300

    Adicionei um efeito de fadeout e fadeIn na imagem

--- script/main.js
+++ script/main.js
@@ -4,6 +4,9 @@ for(let i = 0; i < floates.length; i++){
     let id = floates[i].id
 
     $(`#${id}`).click(({ target })=>{
-        $('#img-produto').attr('src', target.src)
+        $('#img-produto').fadeOut('fast',()=>{
+            $('#img-produto').attr('src', target.src)
+            $('#img-produto').fadeIn();
+        });
     })
 }
